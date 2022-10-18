b695dafa5c8e427c6c096f8e343acc8c2b67cb22
Luciana Santos <luciydss@hotmail.com>
2022-10-18 20:05:59 -0300

    event handlers in MVC
    publisher-subscriver pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -24,6 +24,8 @@ const controlRecipes = async function () {
   }
 };
 
-['hashchange', 'load'].forEach((ev) =>
-  window.addEventListener(ev, controlRecipes),
-);
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes)
+}
+
+init()
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -28,6 +28,12 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
 
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach((ev) =>
+  window.addEventListener(ev, handler),
+);
+  }
+
   #generateMarkup() {
     return `
       <figure class="recipe__fig">
